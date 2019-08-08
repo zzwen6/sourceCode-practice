3a02b6fc2a99d6c504ca0c4c63c5abde707643cd
zhuozhiwen <1104340881@qq.com>
2019-08-08 15:44:02 +0800

    配置的注释

--- mybatis-3/src/main/java/org/apache/ibatis/session/Configuration.java
+++ mybatis-3/src/main/java/org/apache/ibatis/session/Configuration.java
@@ -101,40 +101,103 @@ import org.apache.ibatis.type.TypeHandlerRegistry;
 public class Configuration {
 
   protected Environment environment;
-
+  // 允许在嵌套语句中使用分页 RowBounds，允许为false
   protected boolean safeRowBoundsEnabled;
+
+  // 允许在嵌套语句中使用分页 ResultHandler，允许为false
   protected boolean safeResultHandlerEnabled = true;
+
+  // 下划线转驼峰
   protected boolean mapUnderscoreToCamelCase;
+
+  // 当开启时，任何方法的调用都会加载该对象的所有属性。 否则，每个属性会按需加载（参考 lazyLoadTriggerMethods)。
   protected boolean aggressiveLazyLoading;
+
+  // 是否允许单一语句返回多结果集（需要驱动支持）
   protected boolean multipleResultSetsEnabled = true;
+
+  // 自动生成主键,要驱动支持
   protected boolean useGeneratedKeys;
+
+  // 列标签代替别名，驱动不同表现不同。如oracle同列表现为 xxx_1
   protected boolean useColumnLabel = true;
+
+  // 缓存配置
   protected boolean cacheEnabled = true;
+
+  // 结果集中有null时，是否调用setter。如果是基本类型无效
   protected boolean callSettersOnNulls;
+
+  // 使用实际参数名 为了使用该特性，你的项目必须采用 Java 8 编译，并且加上 -parameters 选项
   protected boolean useActualParamName = true;
+
+  // 当返回行为空时，返回一个空的实例，适用于嵌套的结果集
   protected boolean returnInstanceForEmptyRow;
 
+  // mybatis增加到日志的前缀名
   protected String logPrefix;
+
+  // mybatis 日志默认实现，不指定时自动查找
   protected Class<? extends Log> logImpl;
+
+  // 指定VFS的实现, VFS是mybatis提供的用于访问AS内资源的一个简便接口
   protected Class<? extends VFS> vfsImpl;
+
+  // MyBatis 利用本地缓存机制（Local Cache）防止循环引用（circular references）和加速重复嵌套查询。
+  // 默认值为 SESSION，这种情况下会缓存一个会话中执行的所有查询。
+  // 若设置值为 STATEMENT，本地会话仅用在语句执行上，对相同 SqlSession 的不同调用将不会共享数据。
   protected LocalCacheScope localCacheScope = LocalCacheScope.SESSION;
+
+  // 当没有为参数提供特定的 JDBC 类型时，为空值指定 JDBC 类型。
+  // 某些驱动需要指定列的 JDBC 类型，多数情况直接用一般类型即可，比如 NULL、VARCHAR 或 OTHER。
   protected JdbcType jdbcTypeForNull = JdbcType.OTHER;
+
+  // 指定哪个对象的方法触发一次延迟加载
   protected Set<String> lazyLoadTriggerMethods = new HashSet<>(Arrays.asList("equals", "clone", "hashCode", "toString"));
+
+  // 默认语句超时
   protected Integer defaultStatementTimeout;
+
+  // 为驱动的结果集获取数量设置一个提示值，此参数只可以在查询设置中被覆盖
   protected Integer defaultFetchSize;
+
+  // Specifies a scroll strategy when omit it per statement settings. (Since: 3.5.2)
   protected ResultSetType defaultResultSetType;
+
+  // 配置默认的执行器。SIMPLE 就是普通的执行器；REUSE 执行器会重用预处理语句（prepared statements）； BATCH 执行器将重用语句并执行批量更新。
   protected ExecutorType defaultExecutorType = ExecutorType.SIMPLE;
+
+  // 指定 MyBatis 应如何自动映射列到字段或属性。
+  // NONE 表示取消自动映射；
+  // PARTIAL 只会自动映射没有定义嵌套结果集映射的结果集。
+  // FULL 会自动映射任意复杂的结果集（无论是否嵌套）。
   protected AutoMappingBehavior autoMappingBehavior = AutoMappingBehavior.PARTIAL;
+
+  // 指定发现自动映射目标未知列（或者未知属性类型）的行为。
+  // NONE: 不做任何反应
+  // WARNING: 输出提醒日志 ('org.apache.ibatis.session.AutoMappingUnknownColumnBehavior' 的日志等级必须设置为 WARN)
+  // FAILING: 映射失败 (抛出 SqlSessionException)
   protected AutoMappingUnknownColumnBehavior autoMappingUnknownColumnBehavior = AutoMappingUnknownColumnBehavior.NONE;
 
+  // 属性变量值 settings下的propertis值
   protected Properties variables = new Properties();
+
+  // 反射工厂
   protected ReflectorFactory reflectorFactory = new DefaultReflectorFactory();
+
+  // 对象工厂
   protected ObjectFactory objectFactory = new DefaultObjectFactory();
+
+  // 对象包装工厂
   protected ObjectWrapperFactory objectWrapperFactory = new DefaultObjectWrapperFactory();
 
+  // 延迟加载的全局开关。当开启时，所有关联对象都会延迟加载。特定关联关系中可通过设置fetchType属性来覆盖该项的开关状态。
   protected boolean lazyLoadingEnabled = false;
+
+  // 对象代理
   protected ProxyFactory proxyFactory = new JavassistProxyFactory(); // #224 Using internal Javassist instead of OGNL
 
+  // 数据库厂商标识，如mysql,oracle,sqlserver
   protected String databaseId;
   /**
    * Configuration factory class.
@@ -145,11 +208,20 @@ public class Configuration {
   protected Class<?> configurationFactory;
 
   protected final MapperRegistry mapperRegistry = new MapperRegistry(this);
+
+  // mybatis 插件列表
   protected final InterceptorChain interceptorChain = new InterceptorChain();
+
+  // 类型解析器
   protected final TypeHandlerRegistry typeHandlerRegistry = new TypeHandlerRegistry();
+
+  // 别名
   protected final TypeAliasRegistry typeAliasRegistry = new TypeAliasRegistry();
+
+  // 语言
   protected final LanguageDriverRegistry languageRegistry = new LanguageDriverRegistry();
 
+  // 映射语句
   protected final Map<String, MappedStatement> mappedStatements = new StrictMap<MappedStatement>("Mapped Statements collection")
       .conflictMessageProducer((savedValue, targetValue) ->
           ". please check " + savedValue.getResource() + " and " + targetValue.getResource());
